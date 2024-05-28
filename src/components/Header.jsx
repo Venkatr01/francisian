d254cac79e9527b52ce9e7f7fae380d677778c9e
Venkatr01 <vraju4865@gmail.com>
2024-05-28 23:11:15 +0530

    Add application header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,41 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  // Switch,
+  Route,
+  Link,
+  NavLink
+} from "react-router-dom";
 
 function Header() {
   return (
-    <div className='flex flex-col'>
-        <h1 className='text-blue-800 text-2xl flex justify-center p-2'>Francisian</h1>
-        <div className='flex justify-center ' >
-            <ul className='flex gap-6 justify-center align-middle' >
-            <li>Announcements</li>
-            <li>Timetable</li>
-            <li>Events</li>
-            <li>Marketplace</li>
-            <li>Assignments</li>
-        </ul>
+    // <Router>
+      <div className='flex justify-between align-center p-1 border-solid border-b-2 border-gray-200 '>
+        <div className='flex p-2 text-xl' >
+          <h1 className='  flex justify-center text-black '>Francisian</h1>
         </div>
-    </div>
+          <div className='flex justify-center p-2 text-blue-800' >
+              <ul className='flex gap-6 justify-center align-middle' >
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+              <Link to="/announcements">Announcements</Link>
+              </li>
+              <li>
+              <Link to="/events">Events</Link>
+
+              </li>
+              <li>
+                <NavLink to="/market" >
+                  Market
+                </NavLink>
+              </li>
+              <li>Assignments</li>
+          </ul>
+          </div>
+      </div>
+    // </Router>
   )
 }
 
