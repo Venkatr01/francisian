fe354c481c09b9a42f1613f7a441d55712b59425
Venkatr01 <vraju4865@gmail.com>
2024-05-29 13:43:59 +0530

    Modify code

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <RouterProvider  router={router} /> */}
     <App />
   </React.StrictMode>,
 )
