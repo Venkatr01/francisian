f32171badfc6d1c8291e278334ae50f10d829b3a
Venkatr01 <vraju4865@gmail.com>
2024-05-31 11:05:32 +0530

    Add marketplace product card

--- src/components/Announcements.jsx
+++ src/components/Announcements.jsx
@@ -4,30 +4,29 @@ import Navbar from './Navbar'
 function Announcements() {
   return (
     <div>
-          <div className=' flex p-10 align-middle mt-20 ml-60 bg-gray-200 rounded-3xl mr-80 border-2 border-black' >
-      <span className='bg-red-500 w-3 h-1 p-2 mt-1 rounded-full del text-white animate-ping opacity-70 shadow-orange-600 shadow-2xl'></span>
-      <br />Date: 30-May-2024 <br /> <br />
-      Dear Students,<br/>
-      On 1st June 2024(Saturday) the compensatory classes to all the UG students would be coducted as per Thursday's time table between 8:30am to 1:30pm.
-      <br /><br />Regards,<br /><br />
-      Dr. R N Subba Rao <br /> Principal <br />ST. Francis College
-   </div>
-   <div className=' flex p-10 align-middle mt-20 ml-60 bg-gray-200 rounded-3xl mr-80 border-2 border-black' >
-      <span className='bg-red-500 w-3 h-1 p-2 mt-1 rounded-full del text-white animate-ping opacity-70 shadow-orange-600 shadow-2xl'></span>
-      <br />Date: 30-May-2024 <br /> <br />
-      Dear Students,<br/>
-      On 1st June 2024(Saturday) the compensatory classes to all the UG students would be coducted as per Thursday's time table between 8:30am to 1:30pm.
-      <br /><br />Regards,<br /><br />
-      Dr. R N Subba Rao <br /> Principal <br />ST. Francis College
-   </div>
-   <div className=' flex p-10 align-middle mt-20 ml-60 bg-gray-200 rounded-3xl mr-80 border-2 border-black' >
-      <span className='bg-red-500 w-3 h-1 p-2 mt-1 rounded-2xl del text-white animate-ping opacity-70 shadow-orange-600 shadow-2xl'></span>
-      <br />Date: 30-May-2024 <br /> <br />
-      Dear Students,<br/>
-      On 1st June 2024(Saturday) the compensatory classes to all the UG students would be coducted as per Thursday's time table between 8:30am to 1:30pm.
-      <br /><br />Regards,<br /><br />
-      Dr. R N Subba Rao <br /> Principal <br />ST. Francis College
-   </div>
+         <div className=' flex p-10 align-middle mt-20 ml-60 bg-gray-200 rounded-3xl mr-80 border-2 border-black' >
+         <span className='bg-red-500 w-3 h-1 p-2 mt-1 rounded-full del text-white animate-ping opacity-70 shadow-orange-600 shadow-2xl'></span>
+         <br />Date: 30-May-2024 <br /> <br />
+         Dear Students,<br/>
+         On 1st June 2024(Saturday) the compensatory classes to all the UG students would be coducted as per Thursday's time table between 8:30am to 1:30pm.
+         <br /><br />Regards,<br /><br />
+         Dr. R N Subba Rao <br /> Principal <br />ST. Francis College      </div>
+
+         <div className=' flex p-10 align-middle mt-20 ml-60 bg-gray-200 rounded-3xl mr-80 border-2 border-black' >
+         <span className='bg-red-500 w-3 h-1 p-2 mt-1 rounded-full del text-white animate-ping opacity-70 shadow-orange-600 shadow-2xl'></span>
+         <br />Date: 30-May-2024 <br /> <br />
+         Dear Students,<br/>
+         On 1st June 2024(Saturday) the compensatory classes to all the UG students would be coducted as per Thursday's time table between 8:30am to 1:30pm.
+         <br /><br />Regards,<br /><br />
+         Dr. R N Subba Rao <br /> Principal <br />ST. Francis College      </div>
+         <div className=' flex p-10 align-middle mt-20 ml-60 bg-gray-200 rounded-3xl mr-80 border-2 border-black' >
+         <span className='bg-red-500 w-3 h-1 p-2 mt-1 rounded-2xl del text-white animate-ping opacity-70 shadow-orange-600 shadow-2xl'></span>
+         <br />Date: 30-May-2024 <br /> <br />
+         Dear Students,<br/>
+         On 1st June 2024(Saturday) the compensatory classes to all the UG students would be coducted as per Thursday's time table between 8:30am to 1:30pm.
+         <br /><br />Regards,<br /><br />
+         Dr. R N Subba Rao <br /> Principal <br />ST. Francis College
+      </div>
     </div>
   )
 }
--- src/components/Market.jsx
+++ src/components/Market.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 
 function Market() {
   const product = {
-    image: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1627969658i/58681160.jpg',
-    name: 'Product Name',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/91OlNrX9RbL.png',
+    name: 'The Vikings',
     price: 999,
     stock: 12,
   };
 
   return (
-    <div className="flex gap-7 justify-center items-center min-h-screen">
+      <div className="p-10  grid grid-cols-4  gap-4">
+      <ProductCard product={product} />
+      <ProductCard product={product} />
+      <ProductCard product={product} />
       <ProductCard product={product} />
       <ProductCard product={product} />
       <ProductCard product={product} />
@@ -18,14 +21,14 @@ function Market() {
       <ProductCard product={product} />
 
     </div>
-  );
+    );
 };
 
 
 function ProductCard ({ product }){
   return (
-    <div className="flex flex-col max-w-sm gap-8 rounded-3xl overflow-hidden shadow-lg bg-white">
-      <img className="w-40 rounded-3xl h-30 p-5 mt-3 ml-3 " src={product.image} alt={product.name} />
+    <div className="flex flex-col h-90 w-60 gap-8 rounded-3xl mt-5 overflow-hidden shadow-lg bg-white border-solid border-black">
+      <img className="w-41 rounded-3xl h-64 p-5 mt-3" src={product.image} alt={product.name} />
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2">{product.name}</div>
         <p className="text-gray-700 text-base">Price: ₹{product.price}</p>
@@ -38,6 +41,44 @@ function ProductCard ({ product }){
 // Example usage
 // const App = () => {
   export default Market
+
+//   import React from 'react';
+
+// function Market() {
+//   const product = {
+//     image: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1627969658i/58681160.jpg',
+//     name: 'Product Name',
+//     price: 999,
+//     stock: 12,
+//   };
+
+//   return (
+//     <div className='grid grid-cols-4 gap-4 p-4'>
+//       <ProductCard product={product} />
+//       <ProductCard product={product} />
+//       <ProductCard product={product} />
+//       <ProductCard product={product} />
+//       <ProductCard product={product} />
+//       <ProductCard product={product} />
+//     </div>
+//   );
+// }
+
+// function ProductCard({ product }) {
+//   return (
+    // <div className="flex flex-col max-w-sm gap-8 rounded-3xl overflow-hidden shadow-lg bg-white">
+    //   <img className="w-full rounded-3xl h-64 p-5 mt-3" src={product.image} alt={product.name} />
+    //   <div className="px-6 py-4">
+    //     <div className="font-bold text-xl mb-2">{product.name}</div>
+    //     <p className="text-gray-700 text-base">Price: ₹{product.price}</p>
+    //     <p className="text-gray-700 text-base">Stock left: {product.stock}</p>
+    //   </div>
+    // </div>
+//   );
+// }
+
+// export default Market;
+
 
 
 
