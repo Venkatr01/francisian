d611260c61180c0d200ed0d2e45b7bc3c7d20aa4
Venkatr01 <vraju4865@gmail.com>
2024-05-31 18:42:47 +0530

    Resize announcement msg

--- src/components/Announcements.jsx
+++ src/components/Announcements.jsx
@@ -4,8 +4,8 @@ import Navbar from './Navbar'
 function Announcements() {
   return (
     <div>
-         <div className=' flex p-10 align-middle mt-20 ml-60 bg-white rounded-3xl mr-80 border-2 border-black' >
-         <span className=' w-full h-1 p-2 mt-1 rounded-full del text-white '></span>
+         <div className=' flex p-10 align-middle mt-20 ml-20 bg-white rounded-3xl mr-60 border-2 border-black' >
+         <span className=' w-3 h-1 p-2 mt-1 rounded-full del text-white '></span>
          <br />Date: 30-May-2024 <br /> <br />
          Dear Students,<br/>
          On 1st June 2024(Saturday) the compensatory classes to all the UG students would be coducted as per Thursday's time table between 8:30am to 1:30pm.
